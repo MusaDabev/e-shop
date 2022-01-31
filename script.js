f0c14e7fcd240bd24171db1fb9625a70a8d00b0c
musa_dv <musa_dv@abv.bg>
2022-01-31 21:55:20 +0200

    add hamburger menu btn

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ function showSlides(n) {
 function save() {
   console.log("Saved");
 }
+
+
+const menuBtn = document.querySelector('.menu-btn')
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+}); // end event listener
